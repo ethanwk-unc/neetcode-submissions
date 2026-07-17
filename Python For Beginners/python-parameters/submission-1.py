28d83dfce9d2a73a830a404a19cc16e67fa21173
Ethan <77816049+ethanwk-unc@users.noreply.github.com>
2026-07-17 14:43:57 -0400

    Add: python-parameters - submission-1

---
+++ Python For Beginners/python-parameters/submission-1.py
@@ -0,0 +1,12 @@
+def farewell(text):
+    msg = "Goodbye, " + text
+    print(msg)
+    # print(f"Goodbye, {x}")
+
+
+
+
+# don't modify below this line
+farewell("Bob")
+farewell("Charlie")
+farewell("NeetCode")
